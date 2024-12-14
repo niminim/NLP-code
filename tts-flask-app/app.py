b134404b18ec990abb5209f7c9231f4b8bf334d5
niminim <nimroddaniel1986@gmail.com>
2024-12-14 16:13:58 +0200

    tts flast app - stage 2

--- tts-flask-app/app.py
+++ tts-flask-app/app.py
@@ -57,4 +57,6 @@ def home():
             return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+    # 46.121.216.92
+    # app.run(debug=True, host='0.0.0.0', port=5000)
--- tts-flask-app/tts_model.py
+++ tts-flask-app/tts_model.py
@@ -1,23 +1,23 @@
-# from gtts import gTTS
-#
-#
-# class TTSModel:
-#     def __init__(self):
-#         """
-#         Initialize the TTS model.
-#         You can load a pre-trained model here if required.
-#         """
-#         pass
-#
-#     def generate_audio(self, text, output_path="output.wav"):
-#         """
-#         Generate speech audio from text.
-#         :param text: The input text to be converted to speech.
-#         :param output_path: The file path to save the output audio.
-#         """
-#         # Example using gTTS
-#         tts = gTTS(text)
-#         tts.save(output_path)
+from gtts import gTTS
+
+
+class Basic_TTSModel:
+    def __init__(self):
+        """
+        Initialize the TTS model.
+        You can load a pre-trained model here if required.
+        """
+        pass
+
+    def generate_audio(self, text, output_path="output.wav"):
+        """
+        Generate speech audio from text.
+        :param text: The input text to be converted to speech.
+        :param output_path: The file path to save the output audio.
+        """
+        # Example using gTTS
+        tts = gTTS(text)
+        tts.save(output_path)
 
 
 from TTS.api import TTS
